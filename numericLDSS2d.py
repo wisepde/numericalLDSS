bbb0648b43595c2d246508781e70f1a50c4dd1af
Xiaokai Huo <huox@kl-17119.local>
2019-08-03 02:09:32 +0300

    add 2d

--- numericLDSS2d.py
+++ numericLDSS2d.py
@@ -3,6 +3,7 @@ import numpy as np
 from scipy.optimize import root, fsolve, broyden1
 from functools import partial
 import matplotlib.pyplot as plt
+import pandas as pd
 
 #defined for 2d use
 def Dx(u,h):
@@ -28,10 +29,10 @@ class DLSSsolver2d():
 		self.u = self.u0
 		self.uold = self.u0
 		self.steps = steps
-		if len(savesteps) > 1:
-			self.savesteps = savesteps
+		if type(savesteps) == int:
+			self.savesteps = range(0,steps,savesteps)
 		else:
-			self.savesteps = range(0,steps,savesteps[0])
+			self.savesteps = savesteps
 		self.usol = [self.uold]
 		self.tt = [0]
 	def Nsch(self):
@@ -50,7 +51,7 @@ class DLSSsolver2d():
 		self.u=sol.x.reshape(self.uold.shape)
 	def run(self,method):
 		self.method = method
-		for i in range(self.steps):
+		for i in range(self.steps+1):
 			if method == 'forward_diff':
 				self.forward_diff()
 			elif method == 'explicit_implicit':
@@ -61,6 +62,16 @@ class DLSSsolver2d():
 				self.usol.append(self.u)
 				self.tt.append(self.k*self.steps)
 		self.usol = np.array(self.usol)
+	def saveresult(self, filename = 'a'):
+		print "save results to %s" %filename
+		usolfile = filename + "_sol" + str(self.h) + '.csv'
+		tfile = filename + "_t" + str(self.h) + '.csv'
+		xfile = filename + "_x" + str(self.h) + '.csv'
+		usol2 = [sol.flatten() for sol in self.usol] 
+		pd.DataFrame(usol2).to_csv(usolfile,index=False)
+		pd.DataFrame(self.tt).to_csv(tfile,index=False)
+		pd.DataFrame(self.x).to_csv(xfile,index=False)
+
 	def postposs(self, filename = 'a.png'):
 		print "------Plot Result------"
 		print "solved using %s" % self.method
@@ -74,4 +85,26 @@ class DLSSsolver2d():
 		plt.show()
 
 		print "figure saved to %s" % filename
+def main():
+	# initial parameters
+	epsi = 1e-3
+	m = 8
+	u0_fun =  lambda x: (epsi**0.5 + ((1 + np.cos(2*np.pi*x[0])*np.cos(2*np.pi*x[1]))/2)**m)**2
+	# u0_fun =  lambda x: (epsi**0.5 + ((1 + np.cos(2*np.pi*x[0]))/2)**m)**2
+	# control of the run
+	scope = np.array([[0,1],[0,1]])
+	h = 1e-1
+	k = 1e-10
+	steps = int(8e-6/k)#(7.2e-4/k)
+	plttime =np.array([8e-6])#6, 3.2e-5, 1e-4, 7.2e-4])
+	savesteps = plttime/k
+	savesteps = map(int,savesteps)
+	# build the model
+	a = DLSSsolver2d(k,h,scope,u0_fun,steps,savesteps)
+	# run
+	# a.run('explicit_implicit')
+	a.run('forward_diff')
+	a.saveresult("e2d-3m8")
 
+if __name__ == '__main__':
+	main()
